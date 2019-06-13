b898ca3e07f6f6d934881e0bff461b28264531a2
Lau Llobet <lau.llobet@gmail.com>
2019-06-13 16:09:56 +0200

    add lists of Optionals

--- src/test/scala/Chapter2dot5.scala
+++ src/test/scala/Chapter2dot5.scala
@@ -2,15 +2,19 @@ import cats.Monoid
 import cats.instances.int._    // for Monoid
 import cats.syntax.semigroup._
 import org.scalatest.FlatSpec
-// for Monoid
 
 
 class Chapter2dot5 extends FlatSpec {
 
-  def add(items: List[Int]): Int =
-    items.foldLeft(Monoid[Int].empty)(_ |+| _)
+
+  def add[A](items: List[A])(implicit monoid: Monoid[A]): A =
+    items.foldLeft(monoid.empty)(_ |+| _)
+
+
+  import cats.instances.option._ // for Monoid
 
   it should "add all numbers" in {
     assert( 10 == add(List(1,2,3,4)))
+    assert( Some(10) == add(List(Option(1),None,Option(2),Option(3),Option(4))))
   }
 }
