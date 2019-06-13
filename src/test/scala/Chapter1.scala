7ac02a6b971f7cef86ad7112f7d0a58ba147bf24
Lau Llobet <lau.llobet@gmail.com>
2019-06-13 13:13:25 +0200

    Code feature: prints type class

--- src/test/scala/Chapter1.scala
+++ src/test/scala/Chapter1.scala
@@ -1,13 +1,30 @@
 import org.scalatest.FlatSpec
 
+import PrintableInstances._
+
 class Chapter1 extends FlatSpec  {
 
   behavior of "Chapter1"
 
   it should "SingleHttpGetshould work " in {
-    assert(true == true )
+    assert(PrintableObj.format(123) == "123-" )
   }
-
-
 }
 
+trait Printable[A] {
+  def format(value :A): String
+}
+
+object PrintableObj {
+  def format[A](toFormat: A)(implicit printable: Printable[A]): String = printable.format(toFormat)
+}
+
+object  PrintableInstances {
+  implicit val  PrintableInt: Printable[Int] = new Printable[Int] {
+    override def format(value: Int) = value+"-"
+  }
+
+  implicit val  PrintableString: Printable[String] = new Printable[String] {
+    override  def format(value: String): String = value+"-"
+  }
+}
