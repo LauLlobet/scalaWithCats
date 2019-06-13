be9187985fdbf2f36af91492a45bc022c699669a
Lau Llobet <lau.llobet@gmail.com>
2019-06-13 12:30:09 +0200

    Test suite

---
+++ src/test/scala/Chapter1.scala
@@ -0,0 +1,13 @@
+import org.scalatest.FlatSpec
+
+class Chapter1 extends FlatSpec  {
+
+  behavior of "Chapter1"
+
+  it should "SingleHttpGetshould work " in {
+    assert(true == true )
+  }
+
+
+}
+
